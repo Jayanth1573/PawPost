bad6a99306801bfcbdeaabd6c4a41f201afb88f0
Jayanth <jayanthambaldhage15@gmail.com>
2024-07-29 23:09:34 +0530

    Initial Commit

---
+++ PawPost/PawPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPostApp.swift
+//  PawPost
+//
+//  Created by Jayanth Ambaldhage on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PawPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
