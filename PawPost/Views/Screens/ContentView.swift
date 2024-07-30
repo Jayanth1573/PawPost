e60aba809f28956ea5ba103a2898df7d97dd2604
Jayanth <jayanthambaldhage15@gmail.com>
2024-07-30 10:59:12 +0530

    Initial setup.

---
+++ PawPost/Views/Screens/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  PawPost
+//
+//  Created by Jayanth Ambaldhage on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen 1")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Feed")
+                }
+            Text("Screen 2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("Screen 3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            Text("Screen 4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
+        .accentColor(Color.MyTheme.purpleColor)
+    }
+}
+
+#Preview {
+    ContentView()
+}
